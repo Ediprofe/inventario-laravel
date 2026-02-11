7304aa672dfc0d0624c25c3f09c875e57024bb42
Ediprofe <elprofeedi@gmail.com>
2026-02-10 20:43:50 -0500

    fix sobre carga lenta de admin/items

--- app/Filament/Resources/ItemResource.php
+++ app/Filament/Resources/ItemResource.php
@@ -19,6 +19,13 @@ class ItemResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    // Disable global search for this resource to prevent memory exhaustion
+    // with 5,000+ records and multiple searchable relationship columns
+    public static function getGloballySearchableAttributes(): array
+    {
+        return [];
+    }
+
     public static function form(Form $form): Form
     {
         return $form
